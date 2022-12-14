501b5011c39e499324f645d77c5bbaf89330d3a5
civanozbay <civan417@hotmail.com>
2022-12-14 21:04:49 +0100

    started to TaskController

---
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -0,0 +1,43 @@
+package com.cydeo.controller;
+
+import com.cydeo.entity.ResponseWrapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class TaskController {
+
+    public ResponseEntity<ResponseWrapper> getTasks(){
+
+    }
+
+    public ResponseEntity<ResponseWrapper> getTaskById(){
+
+    }
+
+    public ResponseEntity<ResponseWrapper> createTask(){
+
+    }
+
+    public ResponseEntity<ResponseWrapper> deleteTask(){
+
+    }
+
+    public ResponseEntity<ResponseWrapper> updateTask(){
+
+    }
+
+    public ResponseEntity<ResponseWrapper> employeePendingTasks(){
+
+    }
+
+    public ResponseEntity<ResponseWrapper> employeeUpdateTasks(){
+
+    }
+
+    public ResponseEntity<ResponseWrapper> employeeArchivedTasks(){
+
+    }
+
+}
